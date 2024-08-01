01b0adfd1ca5ac3abd1a0c1903bc6ae133e1a42f
Jani B <aijala.j@gmail.com>
2024-08-01 16:15:01 +0300

    changed some css to effect when input has focus

--- assets/js/mainScripts.js
+++ assets/js/mainScripts.js
@@ -1,31 +1,41 @@
+const colorForRadio = () => {
+  const repaymentRadio = document.getElementById("js-repayment").checked;
+  const interestRadio = document.getElementById("js-interest-only").checked;
+  const repayment = document.getElementById("js-mortage-span-repayment");
+  const interest = document.getElementById("js-mortage-span-interest");
+
+  if (repaymentRadio) {
+    repayment.style.backgroundColor = "hsl(61, 70%, 52%, 0.5)";
+    interest.style.backgroundColor = "hsl(0, 0%, 100%)";
+  } else if (interestRadio) {
+    interest.style.backgroundColor = "hsl(61, 70%, 52%, 0.5)";
+    repayment.style.backgroundColor = "hsl(0, 0%, 100%)";
+  }
+};
+
 const calculateMortage = () => {
   const result = document.getElementById("js-result");
-  const calculatedResult = document.getElementById("js-result-amount");
-  let calculated = 0;
 
   const amount = Number(document.getElementById("amount").value);
-  console.log(amount);
   const term = Number(document.getElementById("term").value);
-  console.log(term);
   const interest = Number(document.getElementById("interest").value);
-  console.log(interest);
-
-  let calc = 300000 * 0.0525 * 25;
-  let calc2 = calc / 25 / 12;
-  let calc3 = (calc - 300000) / 25 / 12;
-
-  let calc4 = 10000 * 0.05 * 5;
-  console.log("total" + calc);
-  console.log("repayment" + calc2);
-  console.log("interest" + calc3);
-  console.log(calc4);
+  const repayment = document.getElementById("js-repayment").checked;
+  const interestOnly = document.getElementById("js-interest-only").checked;
 
   if (amount && term && interest) {
-    calculated = Math.round(
-      (amount * term * (interest / 100)) / term / 12
+    let monthlyinterest = interest / 100 / 12;
+    let totalPayments = term * 12;
+    let shortening = amount / totalPayments;
+    let calculated = Math.round(
+      (amount * ((1 + monthlyinterest) ** totalPayments * monthlyinterest)) /
+        ((1 + monthlyinterest) ** totalPayments - 1)
     ).toFixed(2);
-    totalSum = Math.round(amount * term * (interest / 100)).toFixed(2);
-    return (result.innerHTML = `<div class="results">
+    let calculateInterest = Math.round(
+      calculated - amount / totalPayments
+    ).toFixed(2);
+    let totalSum = Math.round(calculated * 12 * totalPayments).toFixed(2);
+    if (repayment) {
+      return (result.innerHTML = `<div class="results">
         <h2>Your results</h2>
         <p>
          Your results are shown below based on the information you
@@ -39,17 +49,23 @@ const calculateMortage = () => {
         <p>€ ${totalSum} </p>
         </div>
       </div>`);
+    } else if (interestOnly) {
+      return (result.innerHTML = `<div class="results">
+            <h2>Your results</h2>
+            <p>
+             Your results are shown below based on the information you
+              provided. To adjust the results, edit the form and click “calculate
+              repayments” again
+            </p>
+            <div class="final-result">
+            <h3>Your monthly interest </h3>
+            <p>€ ${calculateInterest}</p>
+            </div>
+          </div>`);
+    } else {
+      return (result.innerHTML = "Please check Mortage Type");
+    }
   } else {
     return (result.innerHTML = "Please check the input fields again");
   }
-
-  result.innerHTML = `Your results Your results are shown below based on the information you
-        provided. To adjust the results, edit the form and click “calculate
-        repayments” again. <br>
-        <div>
-        <h2 class=""> </p>
-        Your monthly repayments Total you'll repay over the
-        term`;
-
-  calculatedResult.innerHTML = calculated;
 };
